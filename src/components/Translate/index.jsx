74d1be28d06f7dbd31f95cd95131b3b6351a27e7
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-10-13 12:18:26 +0700

    Refactor: Translate component to use Typography component for translated text

--- src/components/Translate/index.jsx
+++ src/components/Translate/index.jsx
@@ -1,6 +1,7 @@
+import { Typography } from "@mui/material"
 import { useTranslation } from "react-i18next"
 
 export default function Translate({ text }) {
 	const { t } = useTranslation()
-	return t(text)
+	return <Typography>{t(text)}</Typography>
 }
