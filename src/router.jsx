0154f97c45b7abf6244c68cfa67bebb5ec7a91ef
Thanakit Chokbunsuwan <110766946+TeeThanakit@users.noreply.github.com>
2024-11-18 23:01:02 +0700

    feat: init login and register (#1)
    * Refactor: form handling in Login and Register components
    
    * feat: Add react-hook-form dependency
    
    * Refactor: form handling in Login and Register components
    
    * feat: Add react-hook-form dependency
    
    * add: axios dependencies
    
    * init: login and register
    
    * fix: when UI is being loaded
    
    * fix: typo and init interceptors token

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,10 @@
-import { lazy } from "react"
+import { lazy, Suspense } from "react"
 import { Route, Routes } from "react-router-dom"
+import CircularProgress from "@mui/material/CircularProgress"
 
+// Lazy loading will render the components when it's needed
 const Login = lazy(() => import("@/modules/Login"))
+const Register = lazy(() => import("@/modules/Register"))
 const NotFound = lazy(() => import("@/modules/NotFound"))
 const HomePage = lazy(() => import("@/modules/pages/HomePage"))
 const TestPage = lazy(() => import("@/modules/pages/TestPage"))
@@ -10,10 +13,13 @@ const routes = [
 	{ path: "/", element: <HomePage />, index: true },
 	{ path: "/login", element: <Login /> },
 	{ path: "/test", element: <TestPage /> },
+	{ path: "/register", element: <Register /> },
 ]
 
 export default function Router() {
 	return (
+		// Suspense uses to wait when UI is being loaded
+		<Suspense fallback={<CircularProgress />}>
 			<Routes>
 				{routes.map((route) => {
 					return <Route index={route.index} key={route.path} path={route.path} element={route.element} />
@@ -21,5 +27,6 @@ export default function Router() {
 
 				<Route path="*" element={<NotFound />} />
 			</Routes>
+		</Suspense>
 	)
 }
