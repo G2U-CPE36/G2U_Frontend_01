e75ffead03abd7fd7005f4ae1e80d38486d62d86
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-11-24 16:23:59 +0700

    feat: protected route

--- src/router.jsx
+++ src/router.jsx
@@ -1,30 +1,45 @@
 import { lazy, Suspense } from "react"
 import { Route, Routes } from "react-router-dom"
 import CircularProgress from "@mui/material/CircularProgress"
+import { useSelector } from "react-redux"
 
 // Lazy loading will render the components when it's needed
 const Login = lazy(() => import("@/modules/Login"))
 const Register = lazy(() => import("@/modules/Register"))
-const NotFound = lazy(() => import("@/modules/NotFound"))
+const NotFound = lazy(() => import("@/modules/Error/NotFound"))
+const Unauthenticated = lazy(() => import("@/modules/Error/Unauthenticated"))
 const HomePage = lazy(() => import("@/modules/pages/HomePage"))
 const TestPage = lazy(() => import("@/modules/pages/TestPage"))
 const ProductDetail = lazy(() => import("@/modules/pages/Product"))
 
 const routes = [
-	{ path: "/", element: <HomePage />, index: true },
-	{ path: "/login", element: <Login /> },
-	{ path: "/test", element: <TestPage /> },
-	{ path: "/register", element: <Register /> },
-	{ path: "/product/:id", element: <ProductDetail /> },
+	{ path: "/", element: <HomePage />, index: true, isPrivate: false },
+	{ path: "/login", element: <Login />, isPrivate: false },
+	{ path: "/test", element: <TestPage />, isPrivate: false },
+	{ path: "/register", element: <Register />, isPrivate: false },
+	// change ProductDetail isPrivate: to false 
+	{ path: "/product/:id", element: <ProductDetail />, isPrivate: true },
 ]
 
 export default function Router() {
+	const data = useSelector((state) => state.auth)
 	return (
 		// Suspense uses to wait when UI is being loaded
 		<Suspense fallback={<CircularProgress />}>
 			<Routes>
 				{routes.map((route) => {
-					return <Route index={route.index} key={route.path} path={route.path} element={route.element} />
+					if (route.isPrivate) {
+						// I think the token should get from local storage
+						return data?.userToken ? (
+							<Route index={route.index} key={route.path} path={route.path} element={route.element} />
+						) : (
+							<Route key={route.path} path={route.path} element={<Unauthenticated replace />} />
+						)
+					}
+					if (!route.isPrivate) {
+						return <Route index={route.index} key={route.path} path={route.path} element={route.element} />
+					}
+					return null
 				})}
 
 				<Route path="*" element={<NotFound />} />
