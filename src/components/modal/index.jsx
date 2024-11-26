8f2ddfc3c09994b0a9d0b5258d757aebc8fa483c
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-11-27 00:22:10 +0700

    feat: style and add on navigate

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -4,8 +4,15 @@ import Fade from "@mui/material/Fade"
 import Button from "@mui/material/Button"
 import SellIcon from "@mui/icons-material/Sell"
 import SearchIcon from "@mui/icons-material/Search"
+import { useNavigate } from "react-router-dom"
 
 export default function TransitionsModal({ open, onClose }) {
+	const navigate = useNavigate()
+
+	function handleOnClick(path) {
+		onClose()
+		navigate(path)
+	}
 	return (
 		<div>
 			<Modal
@@ -22,18 +29,18 @@ export default function TransitionsModal({ open, onClose }) {
 				}}
 			>
 				<Fade in={open}>
-					<div className="flex absolute top-1/2 left-1/2">
-						<div className="flex flex-col">
-							<Button>
-								<SellIcon sx={{ fontSize: 40 }} />
+					<div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-lg shadow-lg flex gap-6">
+						<div className="flex flex-col items-center">
+							<Button className="hover:bg-gray-100" onClick={() => handleOnClick("/addproduct")}>
+								<SellIcon sx={{ fontSize: 100 }} color="primary" />
 							</Button>
-							<p>Testadwad</p>
+							<p className="mt-2 text-gray-600">Sell</p>
 						</div>
-						<div className="flex flex-col">
-							<Button>
-								<SearchIcon sx={{ fontSize: 40 }} />
+						<div className="flex flex-col items-center">
+							<Button className="hover:bg-gray-100" onClick={() => handleOnClick("/lookingtobuy")}>
+								<SearchIcon sx={{ fontSize: 100 }} color="primary" />
 							</Button>
-							<p>Testadwad</p>
+							<p className="mt-2 text-gray-600">Looking to buy</p>
 						</div>
 					</div>
 				</Fade>
