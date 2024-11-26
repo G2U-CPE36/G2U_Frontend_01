bca39f13b186561cdb8c6a36438435ceba308474
Thanakit Chokbunsuwan <tchokbunsuwan@gmail.com>
2024-11-27 00:49:47 +0700

    fix: Modal div and button styling

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -2,14 +2,14 @@ import Backdrop from "@mui/material/Backdrop"
 import Modal from "@mui/material/Modal"
 import Fade from "@mui/material/Fade"
 import Button from "@mui/material/Button"
-import SellIcon from "@mui/icons-material/Sell"
-import SearchIcon from "@mui/icons-material/Search"
 import { useNavigate } from "react-router-dom"
+import SellOutlinedIcon from "@mui/icons-material/SellOutlined"
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined"
 
 export default function TransitionsModal({ open, onClose }) {
 	const navigate = useNavigate()
-	const styleDiv = "flex flex-col items-center bg-custom-gray rounded-lg border-4 border-main-yellow"
-	const styleButton = { fontSize: 100, color: "white" }
+	const styleDiv = "flex flex-col items-center bg-custom-gray rounded-lg p-5"
+	const styleButton = { fontSize: 150, color: "white" }
 	function handleOnClick(path) {
 		onClose()
 		navigate(path)
@@ -30,16 +30,16 @@ export default function TransitionsModal({ open, onClose }) {
 				}}
 			>
 				<Fade in={open}>
-					<div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-lg shadow-lg flex gap-6">
+					<div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-2 rounded-lg shadow-lg flex gap-2">
 						<div className={styleDiv}>
 							<Button className="hover:bg-gray-100" onClick={() => handleOnClick("/addproduct")}>
-								<SellIcon sx={styleButton} />
+								<SellOutlinedIcon sx={styleButton} />
 							</Button>
 							<p className="mt-2 text-white">Sell</p>
 						</div>
 						<div className={styleDiv}>
 							<Button className="hover:bg-gray-100" onClick={() => handleOnClick("/lookingtobuy")}>
-								<SearchIcon sx={styleButton} color="primary" />
+								<SearchOutlinedIcon sx={styleButton} color="primary" />
 							</Button>
 							<p className="mt-2 text-white">Looking to buy</p>
 						</div>
